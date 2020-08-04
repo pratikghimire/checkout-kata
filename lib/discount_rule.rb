fcd73caf9ef4d5d6f8e8e2906d671fef6ba26434
Pratik Ghimire <pratik.ghimire@shippit.com>
2020-08-04 10:50:36 +1000

    Update discount rules.

--- lib/discount_rule.rb
+++ lib/discount_rule.rb
@@ -1,10 +1,10 @@
 class DiscountRule
   DISCOUNT_RULES = {
-    apple: { },
-    pear: { },
-    banana: { },
-    pineapple: { },
-    mango: { }
+    apple: { package_item: 2.0 },
+    pear: { package_item: 2.0 },
+    banana: { package_item: 2.0 },
+    pineapple: { package_item: 2.0 },
+    mango: { package_item: 4 }
   }
 
   def self.rules
